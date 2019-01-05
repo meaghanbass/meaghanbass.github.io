17140e3a814b9e0c257ec1baa2f74aa17a314aac
Meaghan Bass <45584434+meaghanbass@users.noreply.github.com>
2019-01-05 10:29:54 -0800

    yesphptest

---
+++ yesphp/header.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+	<meta name="author" content="Youth Education Society" />
+	<meta name="copyright" content="Youth Education Society" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="education, san diego, youth, youth education society, yes, nonprofit, non profit, organization, volunteer, california, high school, after school program, inspiration" /> 
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="shortcut icon" href="images/yes_favicon.png" type="image/x-icon" />
+    <title>Education Center SD | San Diego Youth Education Society</title>
+	<!-- <link type="text/css" rel="stylesheet" media="all" href="/modules/system/system-menus.css?e" />-->
+	<link type="text/css" rel="stylesheet" media="all" href="modules/theme/css/style.css" />
+	<!-- <link type="text/css" rel="stylesheet" media="all" href="/sites/all/themes/edc/css/jquery.fancybox.css?e" /> -->
+	<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,200,400italic" rel="stylesheet" type="text/css">
+	<!--	
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script type="text/javascript" src="/misc/jquery.js?e"></script>
+	<script type="text/javascript" src="/misc/drupal.js?e"></script>
+	<script type="text/javascript" src="/sites/all/modules/panels/js/panels.js?e"></script>
+	<script type="text/javascript" src="/sites/all/themes/edc/js/jquery.scrollTo-1.4.2-min.js?e"></script>
+	<script type="text/javascript" src="/sites/all/themes/edc/js/jquery.localscroll-1.2.7-min.js?e"></script>
+	<script type="text/javascript" src="/sites/all/themes/edc/js/jquery.hoverIntent.minified.js?e"></script>
+	<script type="text/javascript" src="/sites/all/themes/edc/js/jquery.fancybox.pack.js?e"></script>
+	<script type="text/javascript">
+	<!--//-->
+
+	  <![CDATA[//><!--
+		jQuery.extend(Drupal.settings, { "basePath": "/" });
+		//--><!]]>
+	
+    <script type="text/javascript" src="/twitter/jquery.tweet.js"></script>
+	<script type="text/javascript" src="/sites/all/themes/edc/js/ready.js"></script>
+    
+  </head>
+
+<body>
+</body>
+</html>
---
+++ yesphp/index.php
@@ -0,0 +1,330 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+<?php 
+include "header.php";
+?>
+
+<body class="subpage wtcsd ">
+<div id="accessibility"><a href="#access-anchor">Skip to Content</a></div>
+<div id="header">
+	<div class="wrapper clear">
+		<div id="block-block-7" class="block block-block">
+
+  <div class="content">
+    <div class="logoholder">
+	<a href="images/mb_sprites.png" id="logo">YES</a></div>
+<ul id="supersocial">
+	<li id="ss-1">
+		<a href="http://eepurl.com/sBUhD" target="_blank">Sign up for our newsletters</a></li>
+	<li id="ss-2">
+		<a href="https://www.facebook.com/SanDiegoRegionalEDC" target="_blank">YES Facebook</a></li>
+	<li id="ss-3">
+		<a href="https://twitter.com/sdregionaledc" target="_blank">YES Twitter</a></li>
+	<li id="ss-4">
+		<a href="http://www.linkedin.com/company/121089" target="_blank">YES LinkedIn</a></li>
+</ul>
+<ul class="clear" id="nav">
+	<li class="mega" id="nav-about">
+		<a class="nav-about-slug menu-slug" href="/region">About Us</a>
+		<div class="menu-dropdown" id="full-menu-about">
+			<ul>
+				<li>
+					<a href="#">Communities</a></li>
+				<li>
+					<a href="#">Lifestyle</a></li>
+				<li>
+					<a href="#">Goals, Objectives and Strategies</a></li>
+			</ul>
+		</div>
+	</li>
+	<li class="mega" id="nav-goglobal">
+		<a class="nav-goglobal-slug menu-slug" href="/wtcsd">Our Services</a>
+		<div class="menu-dropdown" id="full-menu-goglobal">
+			<ul>
+				<li>
+					<a href="/wtcsd/global-trade-investment-initiative">Global Trade &amp; Investment Initiative</a></li>
+				<li>
+					<a href="/wtcsd/goals-objectives-and-strategies">Goals, Objectives and Strategies</a></li>
+				<li>
+					<a href="/wtcsd/why-go-global">Why Go Global?</a></li>
+				<li>
+					<a href="/wtcsd/metroconnect">MetroConnect Program</a></li>
+				<li>
+					<a href="/wtcsd/national-geographic-channels-worlds-smart-cities-san-diego">World's Smart Cities: San Diego</a></li>
+			</ul>
+		</div>
+	</li>
+	<li class="mega" id="nav-key">
+		<a class="nav-key-slug menu-slug" href="/our-economy">Our Economy</a>
+		<div class="menu-dropdown" id="full-menu-key">
+			<ul>
+				<li>
+					<a href="/our-economy/aerospace">Aerospace</a></li>
+				<li>
+					<a href="/our-economy/cleantech">Cleantech</a></li>
+				<li>
+					<a href="/our-economy/craft-beer">Craft Beer</a></li>
+				<li>
+					<a href="/our-economy/cybersecurity">Cybersecurity</a></li>
+				<li>
+					<a href="/our-economy/defense">Defense</a></li>
+				<li>
+					<a href="/our-economy/genomics">Genomics</a></li>
+				<li>
+					<a href="/our-economy/key-universities">Higher Education</a></li>
+				<li>
+					<a href="/our-economy/lifescience">Life Sciences</a></li>
+				<li>
+					<a href="/our-economy/manufacturing">Manufacturing</a></li>
+				<li>
+					<a href="/our-economy/medical-devices">Medical Devices</a></li>
+				<li>
+					<a href="/our-economy/research-institutions">Research Institutions</a></li>
+				<li>
+					<a href="/our-economy/sports-and-active-lifestyle">Sports and Active Lifestyle</a></li>
+				<li>
+					<a href="/our-economy/technology">Technology</a></li>
+				<li>
+					<a href="/our-economy/tourism">Tourism</a></li>
+				<li>
+					<a href="/our-economy/unmanned-systems">Unmanned Systems</a></li>
+			</ul>
+		</div>
+	</li>
+	<li class="mega" id="nav-services">
+		<a class="nav-services-slug menu-slug" href="/services">#</a>
+		<div class="menu-dropdown" id="full-menu-services">
+			<ul>
+				<li>
+					<a href="/services/access">How we help companies</a></li>
+				<li>
+					<a href="/services/research">Research</a></li>
+				<li>
+					<a href="/services/initiatives">Current Initiatives</a></li>
+			</ul>
+		</div>
+	</li>
+	<li class="mega" id="nav-research">
+		<a class="nav-research-slug menu-slug" href="/research-center">Research Center</a>
+		<div class="menu-dropdown" id="full-menu-research">
+			<ul>
+				<li>
+					<a href="/research-center/economic-research">Economic Research</a></li>
+				<li>
+					<a href="/research-center/dashboard">Dashboard</a></li>
+				<li>
+					<a href="/research-center/insight-san-diego">Insight San Diego</a></li>
+			</ul>
+		</div>
+	</li>
+	<li class="mega" id="nav-news">
+		<a class="nav-news-slug menu-slug" href="/news">News &amp; Events</a>
+		<div class="menu-dropdown" id="full-menu-news">
+			<ul>
+				<li>
+					<a href="/news/press">Press Room</a></li>
+				<li>
+					<a href="/news/events">Events</a></li>
+			</ul>
+		</div>
+	</li>
+	
+</ul>
+  </div>
+</div>
+	</div>
+</div>
+<div id="breadcrumb">
+	<div class="wrapper">
+		<div class="breadcrumb"><a href="/">Home</a> <span class="crumbdivider">&gt;</span> 
+			<!-- <a href="/wtcsd" class="active">World Trade Center SD</a> -->
+		</div>	
+	</div>
+</div>
+<div id="global-block-1">
+	<div class="globalwrapper">
+		<img src="images/mb_head_world-trade-center-sd-logo.png" alt="Youth Education Society"/>
+	</div>
+</div>
+<div class="twitterwrap">
+	<div id="twitterfeed" class="tweet"></div>
+</div>
+<div id="global-block-3">
+  <div class="globalwrapper">
+		<img src="images/mb_head_global-image1.png" alt="Invest in Tomorrow San Diego"/>
+		<div class="contra">
+		  <h1>Invest In Tomorrow San Diego</h1>
+		  <h2> Maximize San Diego's economic competitiveness and prosperity through youth education programs</h2>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+<div id="global-block-4">
+	<div class="globalwrapper">
+		<div class="contra">
+		  <h1> Global Education &amp; Opportunity Initiative</h1>
+		  <!--<p>San Diego has global potential. While San Diego is the 17th largest U.S. metro by GDP, it ranks 61st in terms of export intensity and 49th in the U.S. in terms of the percentage of jobs in foreign-owned firms. While many regions would view this as problematic, San Diego sees this as an opportunity.</p> -->
+		  <p>San Diego has global potential. While San Diego is one of the fastest growing metropolitan areas in the U.S., it's high school dropout rate is higher than that of the United States as a whole. While many educators would view this as problematic, we see this as an opportunity.</p>
+		  <!-- <p>San Diego is part of the Global Cities Initiative, a joint project between JPMorgan Chase and the Brookings Institute.</p> -->
+		  <p>San Diego Youth Education Society is partnering with leading local and global innovators to develop new pathways to success for our children. </p>
+		</div>
+        <a href="http://www.sandiegobusiness.org/wtcsd/global-trade-investment-initiative" class="">Learn More</a>
+		<div class="clear"></div>
+	</div>
+</div>
+<div id="global-block-5">
+	<div class="globalwrapper">
+		<img src="images/mb_head_global-image2.png" alt="Youth Education Society"/>
+		<br />
+		<br />
+		<div class="nocenter">
+		<p>MetroConnect&nbsp;provides small and medium enterprises (SMEs) in San Diego with the resources they need to take their business to the rest of the world. MetroConnect provides each selected company up to $10,000 in matching funds to cover up to 50 percent of the costs associated with the company's next step to going global.</p>
+		<p>Thanks to support from the JPMorgan Chase &amp; Co. Foundation, San Diego Regional EDC will award the MetroConnect&nbsp;grant to a handful of companies trying to take their innovative products abroad, secure strategic partnerships, and boost international business that ultimately translates to increased economic activity in San Diego.</p>
+		<p>Thanks to support from all of our partners, the Youth Education Society of San Diego will be hosting several events in the upcoming year for families and children within the county. These events will be focused on inspiring and motivating children and teens to realize their academic passions and future professional pathways.</p>
+		</div>
+		<a href="http://www.sandiegobusiness.org/wtcsd/metroconnect" class="">Learn More</a>
+		<br /><br />
+		<img src="images/head_global-image3.png" alt="Youth Education Society"/>
+	</div>
+</div>
+<div id="global-block-6">
+	<div class="globalwrapper">
+		<img src="images/mb_head_global-image4.png" alt="Roadmap to Success"/>
+		<div class="contra">
+			<h1>
+
+	Roadmap To Success</h1>
+		  <p>A guide to promoting&nbsp;extracurricular learning and identifying pathways to inspire our children.</p>
+			<a href="http://www.sandiegobusiness.org/sites/all/themes/edc/pdf/Go_Global_San_Diego-Export_Resources.pdf" target="_blank" class="">Download PDF</a>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+<div id="global-block-7">
+	<div class="globalwrapper">
+	  <h1>Achievement Challenge</h1>
+	  <div class="nocenter">
+		<p>The Achievement Challenge is an annual event hosted by the Youth Education Society and business, academic, government and civic partners to promote a community of learning and exploration into various fields of learning and future employment. </p>
+
+		<p>Once a year, attendees research and design projects centered around a new field of study that they have learned about. While using applicable skills in a new field, students have the opportunity to create a unique learning experience and be exposed to a variety of new interests. </p>
+
+		</div>		
+	</div>
+</div>
+<div id="global-block-8">
+	<div class="globalwrapper">
+		<h1>Latest News</h1>
+		<div class="view view-goglobalnews view-id-goglobalnews view-display-id-default view-dom-id-1">
+    
+  
+  
+      <div class="view-content">
+        <div class="views-row views-row-1 views-row-odd views-row-first">
+      
+  <div class="views-field-title">
+                <span class="field-content"><a href="/blog/research/san-diegos-economic-pulse-november-2018">San Diego&#039;s Economic Pulse - November 2018</a></span>
+  </div>
+  </div>
+  <div class="views-row views-row-2 views-row-even">
+      
+  <div class="views-field-title">
+                <span class="field-content"><a href="/blog/wtc-san-diego/scientistcom-expands-asia-pacific-region-opens-new-office-tokyo">Scientist.com expands to Asia-Pacific region, opens new office in Tokyo</a></span>
+  </div>
+  </div>
+  <div class="views-row views-row-3 views-row-odd views-row-last">
+      
+  <div class="views-field-title">
+                <span class="field-content"><a href="/blog/wtc-san-diego/rep-peters-wtc-san-diego-trade-mission-japan">Rep. Peters, WTC San Diego to lead trade mission to Japan</a></span>
+  </div>
+  </div>
+    </div>
+  
+  
+  
+  
+  
+  
+</div> 		<div class="clear"></div>
+	</div>
+</div>
+
+
+
+
+
+<div id="footer">
+	<div class="wrapper clear">
+		<div id="block-block-8" class="block block-block">
+
+  <div class="content">
+    <div class="topholder">
+	<a class="top" href="#header">Return to Top</a></div>
+<div class="section-1">
+	<div class="location">
+		<span>Address</span>
+		<p class="address">530 B. Street, 7th Floor<br />
+			San Diego, CA 92101</p>
+		<span>Phone</span>
+		<p>(619) 234-8484</p>
+		<span>Email</span>
+		<p><a href="mailto:contact@youtheducationsociety.org">contact@youtheducationsociety.org</a></p>
+	</div>
+</div>
+<div class="section-2">
+	<ul>
+		<li>
+			<a href="/">YES Home</a></li>
+		<li>
+			<a href="/research">About Us</a></li>
+		<li>
+			<a href="/region">Our Services</a></li>
+		<li>
+			<a href="/wtcsd">Our Economy</a></li>
+		<li>
+			<a href="/news">#</a></li>
+		<li>
+			<a href="/industry">Research Center</a></li>
+		<li>
+			<a href="/get_involved">News &amp; Events</a></li>
+		<li>
+			<a href="/services">#</a></li>
+		<li>
+			<a href="/about">Get Involved</a></li>
+	</ul>
+</div>
+<div class="about-us">
+	<p><a href="http://www.sandiegobusiness.org/made-san-diego"><img class="made-in-san-diego" src="images/made-in-san-diego.png" alt="Made in San Diego" /></a>San Diego Regional EDC's mission is to maximize the region's economic prosperity and global competitiveness.</p>
+</div>
+<div class="section-under clear">
+	<p>&copy;2018 Youth Education Society of San Diego. All rights reserved.   &nbsp;&nbsp;<a href="privacy-policy">Privacy Policy</a></p>
+	<ul>
+		<li id="youtube">
+			<a href="http://www.youtube.com/channel/UC_F7QUX0Lbc4TYGj8kPXh5g" target="_blank">YouTube</a></li>
+		<li id="facebook">
+			<a href="https://www.facebook.com/SanDiegoRegionalEDC" target="_blank">Facebook</a></li>
+		<li id="twitter">
+			<a href="https://twitter.com/sdregionaledc" target="_blank">Twitter</a></li>
+		<li id="linkedin" class="last" target="_blank">
+			<a href="http://www.linkedin.com/company/121089">LinkedIn</a></li>
+	</ul>
+</div>
+  </div>
+</div>
+	</div>
+</div>
+<div id="modal">
+		</div>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33450792-1']);
+  _gaq.push(['_setDomainName', 'sandiegobusiness.org']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+</body>
+</html>
